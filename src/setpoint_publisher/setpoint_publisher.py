03049ab11afb7935caeda51f5f3c5a2cbf280e9f
John Papadakis <jpapadak@uncc.edu>
2017-12-07 17:18:34 -0500

    Adds a tf broadcaster for the active setpoint.

--- src/setpoint_publisher/setpoint_publisher.py
+++ src/setpoint_publisher/setpoint_publisher.py
@@ -9,21 +9,25 @@ class SetpointPublisher:
     def __init__(self):
         self.setpoint_index = 0
         self.setpoint_msg = TransformStamped()
+        self.setpoint_frame = rospy.get_param('~setpoint_frame')
+        self.setpoint_radius = rospy.get_param('~setpoint_radius', 0.1) # meters
+        self.use_tf = rospy.get_param('~use_tf', False)
+
         setpoints_file_path = rospy.get_param('~setpoints_file_path')
         self.setpoints = self.getSetpointsFromFile(setpoints_file_path)
         setpoint_topic = rospy.get_param('~setpoint_topic', 'setpoints')
         self.setpoint_pub = rospy.Publisher(setpoint_topic, TransformStamped, queue_size=10)
-        self.setpoint_radius = rospy.get_param('~setpoint_radius', 0.1) # meters
+        
         init_time = rospy.get_param('~init_time', 0)
         sleep(init_time)
-
-        self.use_tf = rospy.get_param('~use_tf', False)
+        
         if self.use_tf:
-            self.tfparent_frame = rospy.get_param('~tfparent_frame', 'world')
-            self.tfchild_frame = rospy.get_param('~tfchild_frame', 'camera')
+            self.tfparent_frame = rospy.get_param('~tfparent_frame')
+            self.tfchild_frame = rospy.get_param('~tfchild_frame')
             self.tflistener = tf.TransformListener()
+            self.tfbroadcaster = tf.TransformBroadcaster()
         else:
-            pose_topic = rospy.get_param('~pose_topic', 'pose')
+            pose_topic = rospy.get_param('~pose_topic')
             self.pose_sub = rospy.Subscriber(pose_topic, TransformStamped, self.callback)
 
     def getSetpointsFromFile(self, setpoints_file_path):
@@ -51,6 +55,8 @@ class SetpointPublisher:
 
     def updateSetpointMsg(self):
         active_setpoint = self.setpoints[self.setpoint_index]
+        self.setpoint_msg.header.frame_id = self.setpoint_frame
+        self.setpoint_msg.child_frame_id = "setpoint"
         self.setpoint_msg.transform.translation.x = active_setpoint[0]
         self.setpoint_msg.transform.translation.y = active_setpoint[1]
         self.setpoint_msg.transform.translation.z = active_setpoint[2]
@@ -74,16 +80,21 @@ class SetpointPublisher:
         else:
             raise RuntimeError('No tf::Listener has been instantiated!')
 
-    def publishSetpointMsg(self):
-        self.setpoint_msg.header.stamp = rospy.Time.now()
+    def publishSetpoint(self):
+        right_now = rospy.Time.now()
+        self.setpoint_msg.header.stamp = right_now
         self.setpoint_pub.publish(self.setpoint_msg)
+
+        if self.use_tf:
+            active_setpoint = self.setpoints[self.setpoint_index]
+            self.tfbroadcaster.sendTransform(active_setpoint[0:3], active_setpoint[3:], right_now, 'setpoint', self.setpoint_frame)
 
     def run(self):
         while not rospy.is_shutdown():
             if self.use_tf:
                 self.updateUsingTF()
 
-            self.publishSetpointMsg()
+            self.publishSetpoint()
             rospy.Rate(100).sleep()
 
 if __name__ == '__main__':
