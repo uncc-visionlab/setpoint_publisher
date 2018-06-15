2b25364f4834fb2aab08e55569d79b697f513833
arwillis <arwillis@uncc.edu>
2018-06-15 12:11:57 -0500

    Implemented parameter to take in the topic type for topic2tf allowing nav_msgs/Odometry and geometry_msgs/TransformStamped to be routed to TF as a specified frame in the TF tree.

--- src/setpoint_publisher/republish_topic2tf.py
+++ src/setpoint_publisher/republish_topic2tf.py
@@ -14,23 +14,23 @@ class RepublishTopic2TF:
         topic = rospy.get_param('~topic', 'topic')
         topictype = rospy.get_param('~topictype', 'Odometry')
 
+        self.tfparent_frame = rospy.get_param('~tfparent_frame', 'world')
+        self.tfchild_frame = rospy.get_param('~tfchild_frame', 'pose')
+        self.tfbroadcaster = tf2_ros.TransformBroadcaster()
+
         if topictype.lower() == 'transformstamped':
-            rospy.Subscriber(topic, TransformStamped, self.callback)
+            self.pose_sub = rospy.Subscriber(topic, TransformStamped, self.callbackTransformStamped)
         elif topictype.lower() == 'odometry':
-            rospy.Subscriber(topic, Odometry, self.callback)
+            self.pose_sub = rospy.Subscriber(topic, Odometry, self.callbackOdometry)
         else:
             print(rospy.get_name() + ':' + 'Topic type ' + topictype + ' not recognized!\n')
 
-        self.tfparent_frame = rospy.get_param('~tfparent_frame')
-        self.tfchild_frame = rospy.get_param('~tfchild_frame')
-        self.tfbroadcaster = tf2_ros.TransformBroadcaster()
 
-    def callback(self, msg):
+    def callbackTransformStamped(self, msg):
         if not hasattr(self, 'tf_msg'):
             self.tf_msg = TransformStamped()
             self.tf_msg.header.frame_id = self.tfparent_frame
             self.tf_msg.child_frame_id = self.tfchild_frame
-
         if isinstance(msg, TransformStamped):
             self.tf_msg.transform.translation.x = msg.transform.translation.x
             self.tf_msg.transform.translation.y = msg.transform.translation.y
@@ -39,8 +39,17 @@ class RepublishTopic2TF:
             self.tf_msg.transform.rotation.y = msg.transform.rotation.y
             self.tf_msg.transform.rotation.z = msg.transform.rotation.z
             self.tf_msg.transform.rotation.w = msg.transform.rotation.w
+        else:
+            print(rospy.get_name() + ':' + 'Type of message in callback is not recognized!\n')
+        self.tf_msg.header.stamp = rospy.Time.now()                
+        self.republish()
 
-        elif isinstance(msg, Odometry):
+    def callbackOdometry(self, msg):
+        if not hasattr(self, 'tf_msg'):
+            self.tf_msg = TransformStamped()
+            self.tf_msg.header.frame_id = self.tfparent_frame
+            self.tf_msg.child_frame_id = self.tfchild_frame
+        if isinstance(msg, Odometry):
             self.tf_msg.transform.translation.x = msg.pose.pose.position.x
             self.tf_msg.transform.translation.y = msg.pose.pose.position.y
             self.tf_msg.transform.translation.z = msg.pose.pose.position.z
@@ -48,7 +57,8 @@ class RepublishTopic2TF:
             self.tf_msg.transform.rotation.y = msg.pose.pose.orientation.y
             self.tf_msg.transform.rotation.z = msg.pose.pose.orientation.z
             self.tf_msg.transform.rotation.w = msg.pose.pose.orientation.w
-
+        else:
+            print(rospy.get_name() + ':' + 'Type of message in callback is not recognized!\n')
         self.tf_msg.header.stamp = rospy.Time.now()                
         self.republish()
 
